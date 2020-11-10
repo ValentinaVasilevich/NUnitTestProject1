df3711c1ee3d61172c0c1e8b179ac6332a00534b
Valentina Vasilevich <ValentinaVasilevich@coherentsolutions.com>
2020-11-10 11:32:58 +0300

    added var

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -1,9 +1,13 @@
 using NUnit.Framework;
+using System;
 
 namespace NUnitTestProject1
 {
     public class Tests
     {
+        private static String TUT_BY_URL = "https://www.tut.by/";
+        private static String USERNAME = "TestTrenning";
+        private static String PASSWORD = "trenning_001";
         [SetUp]
         public void Setup()
         {
